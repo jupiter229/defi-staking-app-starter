eac660d01d5b38abafe85f17df0cf22f5c7840e0
no-ki <68340397+jupiter229@users.noreply.github.com>
2021-12-03 06:43:28 -0800

    minor fix on sol files, create migration js

---
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+contract Tether {
+    string public name = 'Tether';
+    string symbol = 'USDT';
+    uint256 public totalSuppy = 1000000000000000000; // 1 million tokens;
+    uint decimals = 18;
+}
