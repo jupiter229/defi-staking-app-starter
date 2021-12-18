d9c56e92a61a848828df9b656438fd90bdf9495e
jupiter229 <smartwang1004@outlook.com>
2021-12-18 07:40:40 -0800

    deploy, compile and debugging, migration

--- defi-staking-app-starter/src/contracts/RWD.sol
+++ defi-staking-app-starter/src/contracts/RWD.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 contract RWD {
     string public name = 'Reward Token';
     string symbol = 'RWD';
-    uint256 public totalSupply = 1000000000000000000; // 1 million tokens;
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens;
     uint decimals = 18;
 
     event Transfer(
--- defi-staking-app-starter/src/contracts/Tether.sol
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 contract Tether {
     string public name = 'Mock Tether Token';
     string symbol = 'USDT';
-    uint256 public totalSupply = 1000000000000000000; // 1 million tokens;
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens;
     uint decimals = 18;
 
     event Transfer(
