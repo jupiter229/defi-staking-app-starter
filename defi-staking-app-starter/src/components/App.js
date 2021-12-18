31b470af24ef1c0e42c41c1fdd632b2777696930
jupiter229 <smartwang1004@outlook.com>
2021-12-18 10:31:03 -0800

    add metamask web3

--- defi-staking-app-starter/src/components/App.js
+++ defi-staking-app-starter/src/components/App.js
@@ -1,9 +1,33 @@
 import React, {Component} from 'react'
 import './App.css'
 import Navbar from './Navbar';
+import Web3 from 'web3';
 
 class App extends Component {
     // Our react code goes here
+
+    async UNSAFE_componentWillMount() {
+        await this.loadWeb3()
+        await this.loadBlockchainData()
+    }
+
+    async loadWeb3() {
+        if(window.ethereum) {
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+        }else if(window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider)
+        }else{
+            window.alert('No ethereum browser detected! Checkout MetaMask!');
+        }
+    }
+
+    async loadBlockchainData() {
+        const web3 = window.web3;
+        const account = await web3.eth.getAccounts();
+        console.log('account ', account);
+    }
+
     constructor(props) {
         super(props)
         this.state = {
@@ -16,7 +40,7 @@ class App extends Component {
             <div>
                 <Navbar account={this.state.account}/>
                 <div className="text-center">
-                    <h>hello world</h>
+                    <h1>hello world</h1>
                 </div>
             </div>
             
