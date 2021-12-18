b5dbc766bf22c99823046248b2343d497c479776
jupiter229 <smartwang1004@outlook.com>
2021-12-18 06:15:59 -0800

    add transform, approval function and event

--- defi-staking-app-starter/src/contracts/Tether.sol
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -9,30 +9,48 @@ contract Tether {
     event Transfer(
         address indexed _from,
         address indexed _to,
-        uint _value
-    )
+        uint256 _value
+    );
 
-    event Approve (
+    event Approval (
         address indexed _owner,
-        address indexed _spended,
-        uint _value
+        address indexed _spender,
+        uint256 _value
     
     );
 
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256))  public allowance;
 
     constructor() {
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint _value ) public returns (bool success) {
+    function transfer(address _to, uint256 _value ) public returns (bool success) {
         // require that the value is greater or equal for transfer
-        require(balanceOf[msg.sender] > = _value);
+        require(balanceOf[msg.sender] >= _value);
         // transfer the amount and sub the balance from owner
         balanceOf[msg.sender] -= _value;
         // add the balance
         balacneOf[_to] += value;
-        emit Transfer(_from, _to, _value);
+        emit Transfer(msg.sender, _to, _value);
         return;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    
+    function transferFrom(address _from, address _to, uint2256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        balanceOf[_to] += _value;
+
+        balanceOf[_from] -= _value;
+        allowance[from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
     }
 }
