79e384a2ac249e252d481521f67b9539b6f19577
jupiter229 <smartwang1004@outlook.com>
2021-12-18 06:39:10 -0800

    fix bugs, and deploy

--- defi-staking-app-starter/migrations/2_deploy_contracts.js
+++ defi-staking-app-starter/migrations/2_deploy_contracts.js
@@ -1,5 +1,14 @@
 const Tether = artifacts.require('Tether');
+const RWD = artifacts.require('RWD');
+const DecentralBank = artifacts.require('DecentralBank');
 
 module.exports = async function (deployer) {
+    //Deploy Mock Tether Contract
     await deployer.deploy(Tether);
+
+    //Deploy RWD Contract
+    await deployer.deploy(RWD);
+
+    //Deploy DecentralBank Contract
+    await deployer.deploy(DecentralBank);
 }
--- defi-staking-app-starter/src/contracts/RWD.sol
+++ defi-staking-app-starter/src/contracts/RWD.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 contract RWD {
     string public name = 'Reward Token';
     string symbol = 'RWD';
-    uint256 public totalSuppy = 1000000000000000000; // 1 million tokens;
+    uint256 public totalSupply = 1000000000000000000; // 1 million tokens;
     uint decimals = 18;
 
     event Transfer(
@@ -22,7 +22,7 @@ contract RWD {
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256))  public allowance;
 
-    constructor() {
+    constructor() public {
         balanceOf[msg.sender] = totalSupply;
     }
 
@@ -32,9 +32,9 @@ contract RWD {
         // transfer the amount and sub the balance from owner
         balanceOf[msg.sender] -= _value;
         // add the balance
-        balacneOf[_to] += value;
+        balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
-        return;
+        return true;
     }
 
     function approve(address _spender, uint256 _value) public returns (bool success) {
@@ -43,13 +43,13 @@ contract RWD {
         return true;
     }
     
-    function transferFrom(address _from, address _to, uint2256 _value) public returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(_value <= balanceOf[_from]);
         require(_value <= allowance[_from][msg.sender]);
         balanceOf[_to] += _value;
 
         balanceOf[_from] -= _value;
-        allowance[from][msg.sender] -= _value;
+        allowance[_from][msg.sender] -= _value;
         emit Transfer(_from, _to, _value);
         return true;
     }
