db56e65d32f68cf5de8f087d0653ddb3fbc490bb
jupiter229 <smartwang1004@outlook.com>
2021-12-18 08:57:02 -0800

    add stacking function

--- defi-staking-app-starter/src/contracts/DecentralBank.sol
+++ defi-staking-app-starter/src/contracts/DecentralBank.sol
@@ -9,8 +9,34 @@ contract DecentralBank {
     Tether public tether;
     RWD public rwd;
 
+    address[] public stackers;
+
+    mapping(address => uint) public stackingBalance;
+    mapping(address => bool) public hasStacked;
+    mapping(address => bool) public isStacked;
+
     constructor(RWD _rwd, Tether _tether) public {
         rwd = _rwd;
         tether = _tether;
+    }
+
+    // stacking function
+    function depositTokens(uint _amount) public {
+        // require stacking amount to be greater than zero
+        require(_amount > 0, 'amount cannot be 0');
+        
+        //Transfer tether tokens to this contract address for stacking
+        tether.transferFrom(msg.sender, address(this), amount);
+        
+        // Update Stacking Balance
+        stackingBalance(msg.sender) = stackingBalance(msg.sender) + _amount;
+
+        if(!hasStacked) {
+            stackers.push(msg.sender);
+        }
+
+        //Update Stacking Balance
+        isStacked[msg.sender] = true;
+        hasStacked[msg.sender] = true;
     }
 }
